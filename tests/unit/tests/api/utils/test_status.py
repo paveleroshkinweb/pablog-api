21c33e0c368d020fa7139019047676a2799480c5
paveleroshkinweb <eroshkin321@gmail.com>
2023-12-14 22:23:05 +0100

    fix tests, remove request-id from node nginx config

--- tests/unit/tests/api/utils/test_status.py
+++ tests/unit/tests/api/utils/test_status.py
@@ -8,5 +8,3 @@ PING_PATH = "/status"
 async def test_status(client_v1: AsyncClient):
     response = await client_v1.get(PING_PATH)
     assert response.status_code == status.HTTP_200_OK
-    assert response.headers.get("X-Request-Id") is not None
-    assert response.headers.get("X-Process-Time") is not None
