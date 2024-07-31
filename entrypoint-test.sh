a6435fabea2322c6dd1d813e0ca2fefc716576f1
paveleroshkinweb <eroshkin321@gmail.com>
2024-07-31 10:31:05 +0200

    run migrations before tests

--- entrypoint-test.sh
+++ entrypoint-test.sh
@@ -10,5 +10,6 @@ TEST_TYPE=$1
 if [ "$TEST_TYPE" = "unit" ]; then
   poetry run pytest tests/unit
 elif [ "$TEST_TYPE" = "integration" ]; then
+  poetry run alembic upgrade head
   poetry run pytest tests/integration
 fi
