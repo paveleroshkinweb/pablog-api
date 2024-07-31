47128f5f162611b40ac21fa929a571d6a81a249f
paveleroshkinweb <eroshkin321@gmail.com>
2024-07-31 19:53:22 +0200

    add current session method for db manager

--- pablog_api/database/db_manager.py
+++ pablog_api/database/db_manager.py
@@ -24,7 +24,7 @@ class MasterSlaveManager:
         )
 
     @asynccontextmanager
-    async def master_session(self, nested=False):
+    async def master_session(self, nested: bool = False):
         async with self.scoped_session(use_master=True) as session:
             try:
                 if nested and session.in_transaction():
@@ -38,6 +38,27 @@ class MasterSlaveManager:
     @asynccontextmanager
     async def slave_session(self):
         async with self.scoped_session(use_master=False) as session:
+            try:
+                yield session
+            finally:
+                await session.close()
+
+    @asynccontextmanager
+    async def current_session(self, use_master_if_missing: bool = True, nested: bool = False):
+        session = self.scoped_session.registry.registry.get(bind_session_to_request_id())
+
+        if not session:
+
+            if use_master_if_missing:
+                async with self.master_session(nested) as master_session:
+                    yield master_session
+
+            else:
+                async with self.slave_session() as slave_session:
+                    yield slave_session
+
+        else:
+
             try:
                 yield session
             finally:
