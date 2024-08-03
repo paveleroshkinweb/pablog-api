de31046f72ae87b890d293b404fe429071930454
paveleroshkinweb <eroshkin321@gmail.com>
2024-08-03 14:18:08 +0200

    add periodic check from database for config

--- pablog_api/apps/hot_config/core/config.py
+++ pablog_api/apps/hot_config/core/config.py
@@ -1,4 +1,5 @@
-from asyncio import Lock
+import asyncio
+import random
 
 from pablog_api.apps.hot_config.constant import CLUSTER_NOTIFICATION_CHANNEL
 from pablog_api.apps.hot_config.database import get_configuration_repository
@@ -11,12 +12,23 @@ import structlog
 
 logger = structlog.get_logger(__name__)
 
-CONFIG = None
-_CONFIG_LOCK = Lock()
+# Use random so different workers ping database in different time and don't overwhelm database
+_DB_PERIOD_CHECK: int = 120 + random.randint(1, 60)  # nosec
+
+_CONFIG: None | ConfigurationSchema = None
+
+_CONFIG_LOCK: asyncio.Lock = asyncio.Lock()
 
 
 async def subscribe_to_config_updates():
-    global CONFIG
+    asyncio.create_task(subscribe_to_redis_notifications())
+    asyncio.create_task(periodic_check_in_db())
+
+
+async def subscribe_to_redis_notifications():
+    global _CONFIG
+
+    logger.info("Subscribing to redis notifications")
 
     redis_cluster = get_redis_cluster()
     pubsub = redis_cluster.pubsub()
@@ -29,38 +41,60 @@ async def subscribe_to_config_updates():
 
         logger.info(f"Received notification about new config id = {new_config_id}")
 
-        if CONFIG and CONFIG.id >= new_config_id:
-            logger.info(f"Skipping update as current version is bigger: {CONFIG.id} >= {new_config_id}")
+        if _CONFIG and _CONFIG.id >= new_config_id:
+            logger.info(f"Skipping update as current version is bigger: {_CONFIG.id} >= {new_config_id}")
             continue
 
         async with _CONFIG_LOCK:
-            logger.info("Updating config from database")
-            db_manager = get_db_manager()
             try:
-                async with db_manager.master_session() as session:
-                    configuration_repository = get_configuration_repository(session)
-                    db_config = await configuration_repository.get_last_config()
-                    if not db_config:
-                        logger.error("Config was deleted from db?")
-                        continue
-                    if CONFIG.id >= db_config.id:
-                        logger.info(f"Config was already updated to latest: {CONFIG.id} >= {db_config.id}")
-                    else:
-                        new_config_schema = ConfigurationSchema(**{
-                            'id': db_config.id,
-                            'data': db_config.data
-                        })
-
-                        CONFIG.id = new_config_schema.id
-                        CONFIG.data = new_config_schema.data
-
-                        logger.info(f"New config version = {CONFIG.id}")
+                await _update_config_from_db()
             except Exception as e:
                 logger.error(f"Unexpected error happened during config update from db: {e}")
 
 
+async def periodic_check_in_db():
+    global _CONFIG
+
+    logger.info("Subscribing to database changes")
+
+    while True:
+
+        await asyncio.sleep(_DB_PERIOD_CHECK)
+
+        async with _CONFIG_LOCK:
+            try:
+                await _update_config_from_db()
+            except Exception as e:
+                logger.error(f"Unexpected error happened during config update from db: {e}")
+
+
+async def _update_config_from_db():
+    global _CONFIG
+
+    logger.info("Updating config from database")
+
+    db_manager = get_db_manager()
+    async with db_manager.master_session() as session:
+        configuration_repository = get_configuration_repository(session)
+        db_config = await configuration_repository.get_last_config()
+
+        if _CONFIG.id >= db_config.id:
+            logger.info(f"Config was already updated to latest: {_CONFIG.id} >= {db_config.id}")
+            return
+
+        new_config_schema = ConfigurationSchema(**{
+            'id': db_config.id,
+            'data': db_config.data
+        })
+
+        _CONFIG.id = new_config_schema.id
+        _CONFIG.data = new_config_schema.data
+
+        logger.info(f"New config version = {_CONFIG.id}")
+
+
 async def set_config_from_db():
-    global CONFIG
+    global _CONFIG
 
     logger.info("Setting initial config from database")
 
@@ -69,11 +103,11 @@ async def set_config_from_db():
         configuration_repository = get_configuration_repository(session)
         db_config = await configuration_repository.get_last_config()
 
-        CONFIG = ConfigurationSchema(**{
+        _CONFIG = ConfigurationSchema(**{
             'id': db_config.id,
             'data': db_config.data
         })
 
 
 def get_hot_config():
-    return CONFIG
+    return _CONFIG
