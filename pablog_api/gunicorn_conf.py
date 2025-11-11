78d95a2bd7e5adc9e6e9022a189c75f10e0172f4
paveleroshkinweb <eroshkin321@gmail.com>
2025-11-11 20:10:30 +0100

    allow to override default thread pool size

--- pablog_api/gunicorn_conf.py
+++ pablog_api/gunicorn_conf.py
@@ -1,3 +1,6 @@
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
+
 from pablog_api.settings.app import get_app_settings
 
 from uvicorn.workers import UvicornH11Worker
@@ -11,7 +14,7 @@ settings = get_app_settings()
 
 bind = settings.service_settings.dsn()
 
-workers = settings.service_settings.workers
+workers = settings.service_settings.max_workers
 
 worker_class = "pablog_api.gunicorn_conf.HeadlessUvicornWorker"
 
@@ -37,3 +40,16 @@ loglevel = settings.logging.log_level.value.lower()
 capture_output = True
 
 logconfig_dict = settings.logging.get_config(settings.environment)
+
+
+def post_fork(server, worker):
+    thread_pool_executor = ThreadPoolExecutor(max_workers=settings.service_settings.max_threads_per_worker)
+    loop = asyncio.get_event_loop()
+    loop.set_default_executor(thread_pool_executor)
+    worker._thread_pool_executor = thread_pool_executor
+
+
+def worker_exit(server, worker):
+    if hasattr(worker, "_thread_pool_executor"):
+        worker._thread_pool_executor.shutdown(wait=True)
+        worker._thread_pool_executor = None
--- pablog_api/settings/service.py
+++ pablog_api/settings/service.py
@@ -11,7 +11,9 @@ class ServiceSettings(BaseAppSettings):
 
     api_port: int = pydantic.Field(default=8001)
 
-    workers: int = pydantic.Field(default=multiprocessing.cpu_count())
+    max_workers: int = pydantic.Field(default=multiprocessing.cpu_count())
+
+    max_threads_per_worker: int = pydantic.Field(default=multiprocessing.cpu_count())
 
     pidfile: str = pydantic.Field(default="/var/run/pablog.pid")
 
